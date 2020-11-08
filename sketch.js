46f5c6efec1cd6d8a7319a9fae05ce74a1a489ba
jaseer123 <73378096+jaseer123@users.noreply.github.com>
2020-11-08 12:26:34 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,35 @@
+var car,wall
+var speed,weight
+var defromation
+
+function setup() {
+  createCanvas(1500,400);
+ car= createSprite(50, 200, 50, 50);
+
+ 
+ wall=createSprite(900,200,600,height/2)
+ wall.shapeColor=color(80,80,80)
+ speed=random(55,90)
+ weight=random(400,1500)
+ car.velocityX=speed
+}
+
+function draw() {
+  background(255,255,255);  
+  if(wall.x-car.x<(car.width+wall.width)/2){
+    car.velocityX=0
+  }
+  defromation= 0.5*weight*speed*speed/22509
+ 
+  if(defromation>180){
+    car.shapeColor=color(255,0,0)
+  }
+  if(defromation<180 && defromation>100){
+    car.shapeColor=color(230,230,0)
+  }
+  if(defromation<100){
+    car.shapeColor=color(0,255,0)
+  }
+
+  drawSprites();
+}
